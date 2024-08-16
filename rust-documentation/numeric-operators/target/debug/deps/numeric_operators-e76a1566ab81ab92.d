a1891311be4d34df9ebc9b24f3be294e1c0c56e2
j-moreno-c-r <joaomcr@proton.me>
2024-08-16 14:29:32 -0300

    numeric operators

---
+++ rust-documentation/numeric-operators/target/debug/deps/numeric_operators-e76a1566ab81ab92.d
@@ -0,0 +1,5 @@
+/home/joaozinho/rust_lilte_init/rust-documentation/numeric-operators/target/debug/deps/numeric_operators-e76a1566ab81ab92: src/main.rs
+
+/home/joaozinho/rust_lilte_init/rust-documentation/numeric-operators/target/debug/deps/numeric_operators-e76a1566ab81ab92.d: src/main.rs
+
+src/main.rs:
---
+++ rust-documentation/numeric-operators/target/debug/numeric-operators.d
@@ -0,0 +1,1 @@
+/home/joaozinho/rust_lilte_init/rust-documentation/numeric-operators/target/debug/numeric-operators: /home/joaozinho/rust_lilte_init/rust-documentation/numeric-operators/src/main.rs
