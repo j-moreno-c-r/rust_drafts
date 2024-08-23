d1da10093cfc2788a6a7f55301d3613a980eb96d
j-moreno-c-r <joaomcr@proton.me>
2024-08-22 21:27:44 -0300

    flow...

---
+++ rust-documentation/flow/target/debug/deps/flow-68437e0fd9b02ca7.d
@@ -0,0 +1,5 @@
+/home/joaozinho/rust_lilte_init/rust-documentation/flow/target/debug/deps/flow-68437e0fd9b02ca7: src/main.rs
+
+/home/joaozinho/rust_lilte_init/rust-documentation/flow/target/debug/deps/flow-68437e0fd9b02ca7.d: src/main.rs
+
+src/main.rs:
---
+++ rust-documentation/flow/target/debug/flow.d
@@ -0,0 +1,1 @@
+/home/joaozinho/rust_lilte_init/rust-documentation/flow/target/debug/flow: /home/joaozinho/rust_lilte_init/rust-documentation/flow/src/main.rs
